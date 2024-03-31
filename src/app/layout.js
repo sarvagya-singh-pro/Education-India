73ec485196601176b9b02d6fa62dd5f5f7b5f2a8
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2024-03-31 18:53:00 +0530

    Revert "feat: login signup front-end "

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
-
-import { CssBaseline } from '@mui/material';
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      < CssBaseline />
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
